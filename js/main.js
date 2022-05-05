5d5c999b9916aa4ef75032ed7b0bce00ef37cc81
Akhgofur <gofurbekaxmadjonov397@gmail.com>
2022-05-05 17:52:14 +0500

    added home page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const elCard = document.querySelector('.info__card')
 const elCardBtn = document.querySelectorAll('.info__card-btn')
+const elHeaderNav = document.querySelector('.header-links')
+const elHeaderBtn = document.querySelector('.header-toggler-js')
 
+elHeaderBtn.addEventListener('click', () => {
+  elHeaderNav.classList.toggle('header-links--open')
+})
 elCardBtn.forEach(btn => {
   btn.addEventListener('click', () => {
     btn.closest('.info__card').classList.toggle('info__card-back-side')
